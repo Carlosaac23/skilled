dfaebfb66006c297bda9c34d658e452336d2d0ac
Carlos <carlosaac232001@gmail.com>
2026-04-19 17:15:07 -0500

    refactor: make handleCopy function asynchronous with error handling

--- src/hooks/useCopy.ts
+++ src/hooks/useCopy.ts
@@ -3,10 +3,14 @@ import { useState } from 'react';
 export function useCopy(textToCopy: string) {
   const [copied, setCopied] = useState(false);
 
-  function handleCopy() {
-    navigator.clipboard.writeText(textToCopy);
-    setCopied(true);
-    setTimeout(() => setCopied(false), 2000);
+  async function handleCopy() {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch {
+      setCopied(false);
+    }
   }
 
   return { copied, handleCopy };
